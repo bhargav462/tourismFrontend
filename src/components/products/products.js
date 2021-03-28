eae5c2cb5b5b370a35ac0502705a3e44c362557f
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2021-03-28 15:26:02 +0530

    adding filter tp the package page

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -5,11 +5,32 @@ import land1 from './../../assets/images/tajmahalp1.jpg';
 import land2 from './../../assets/images/tajmahalp2.jpg';
 import land3 from './../../assets/images/tajmahalp3.jpg';
 import land4 from './../../assets/images/tajmahalp1.jpg';
+import Dropdown from './../../utils/dropdown/dropdown'
 
 class CardLayout extends Component{
+
+    itemList = ["Agra","Jaipur","Delhi"];
+
     render() {
         return (
             <div className={classes["container"]}>
+
+                <div className={classes["filter"]}>
+                    <div className={classes["city"]}>
+                        <select className={classes["dropdown"]}>
+                            {
+                                this.itemList.map((item,index) => {
+                                    return <option key={index}>{item}</option>;
+                                })
+                            }
+                        </select>
+                        <input min="0" placeholder="No. of days(min)" className={[classes["input"],classes["number"]].join(' ')} type="number" required />
+                        <input min="0" placeholder="No. of days(max)" className={[classes["input"],classes["number"]].join(' ')} type="number" required />
+                        <input placeholder="Month of Travel" className={classes["input"]} type="number" required />
+                        <button className={classes["searchButton"]}>Search</button>
+                    </div> 
+                </div>
+
             <main className={classes.CardLayout}>
                 <article>
                     <Link to="/package/agra/details">
